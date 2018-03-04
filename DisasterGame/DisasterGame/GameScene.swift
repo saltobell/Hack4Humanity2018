9ada40574a9ee6ba7ae7815f4b43d2ad6fb709f7
Jacqueline Reardon <jreardon@Jacquelines-MacBook-Pro.local>
2018-03-03 19:45:11 -0800

    Sprite1

--- DisasterGame/DisasterGame/GameScene.swift
+++ DisasterGame/DisasterGame/GameScene.swift
@@ -7,104 +7,92 @@
 //
 
 import SpriteKit
-import GameplayKit
+
+private let kAnimalNodeName = "movable"
 
 class GameScene: SKScene {
     
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
+    let food = SKSpriteNode(imageNamed: "food")
+    let water_gallon = SKSpriteNode(imageNamed: "water_gallon")
+    let flashlight = SKSpriteNode(imageNamed: "flashlight")
+    let radio = SKSpriteNode(imageNamed: "radio")
+    let medicine = SKSpriteNode(imageNamed: "medicine")
+    let hand_sanitizer = SKSpriteNode(imageNamed: "hand_sanitizer")
+    let box = SKSpriteNode(imageNamed: "box")
+    let batteries = SKSpriteNode(imageNamed: "batteries")
+    let first_aid_kit = SKSpriteNode(imageNamed: "first_aid_kit")
     
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var selectedNode = SKSpriteNode()
     
-    override func sceneDidLoad() {
-
-        self.lastUpdateTime = 0
+    override func didMove(to: SKView) {
+        self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        box.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        box.position = CGPoint(x: 0, y: 0)
+        box.setScale(0.5)
+        box.name = kAnimalNodeName
+        food.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        food.position = CGPoint(x: 100, y: 100)
+        food.name = kAnimalNodeName
+        self.addChild(box)
+        self.addChild(food)
+    }
+    
+    func selectNodeForTouch(touchLocation: CGPoint) {
+        // 1
+        let touchedNode = self.atPoint(touchLocation)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+        if touchedNode is SKSpriteNode {
+            // if you dont touch a node, do nothing
+            if !selectedNode.isEqual(touchedNode) {
+                selectedNode.removeAllActions()
+                selectedNode.run(SKAction.rotate(toAngle: 0.0, duration: 0.1))
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
+                selectedNode = touchedNode as! SKSpriteNode
+                
+                // 3
+                if touchedNode.name! == kAnimalNodeName {
+                    let sequence = SKAction.sequence([SKAction.rotate(byAngle: degToRad(degree: -4.0), duration: 2),
+                                                      SKAction.rotate(byAngle: 0.0, duration: 0.1),
+                                                      SKAction.rotate(byAngle: degToRad(degree: 4.0), duration: 2)])
+                    selectedNode.run(SKAction.repeatForever(sequence))
+                }
+            }
         }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        let touch = touches.first as? UITouch!
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        let positionInScene = touch?.location(in: self.view)
+        selectNodeForTouch(touchLocation: positionInScene!)
+        
+        //selectNodeForTouch(touchLocation: CGPoint(positionInScene))
     }
+    
+    func degToRad(degree: Double) -> CGFloat {
+        return CGFloat(Double(degree) / 180.0 * M_PI)
+    }
+    
+
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        
+        for touch in touches {
+            let location = touch.location(in: self)
+            selectedNode.position.x = location.x
+            selectedNode.position.y = location.y
+//                box.position.x = box_location.x
+//                box.position.y = box_location.y
+//
+//
+//            let food_location = touch.location(in: self)
+//            food.position.x = food_location.x
+//            food.position.y = food_location.y
+        }
     }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
         
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
-        }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
     }
 }
